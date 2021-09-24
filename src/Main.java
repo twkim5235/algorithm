902d6fb2faf92783bca62010af2d9f572de03ba4
twkim5235 <sssunggure@naver.com>
2021-09-24 20:33:39 +0900

    7-9 Tree node에서 말단 Node까지 가장 짧은 거리 구하기

--- src/Main.java
+++ src/Main.java
@@ -22,11 +22,20 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
 
-        int s = sc.nextInt();
-        int e = sc.nextInt();
-        Main T = new Main();
-        System.out.println(T.findCalfBFS(s, e));
+        Main tree = new Main();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.getShortestDistanceDfs(0, tree.root));
 
+    }
+
+    //7-9 Tree Node에서 말단 Node까지 가장 짧은 거리 구하기(DFS)
+    public int getShortestDistanceDfs(int L, Node root) {
+        if (root.lt == null && root.rt == null) return L;
+        else return Math.min(getShortestDistanceDfs(L + 1, root.lt), getShortestDistanceDfs(L + 1, root.rt));
     }
 
     //7-8 송아지 찾기
